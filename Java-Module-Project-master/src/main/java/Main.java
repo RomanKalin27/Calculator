dde48aefdc693e5432fd0bdc731069657646d91e
RomanKalin27 <kalinroman034@gmail.com>
2022-09-25 18:14:16 +0300

    й

---
+++ Java-Module-Project-master/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("На сколько человек вы хотели бы разделить чек?");
+        int numPeople = scan.nextInt();
+        while (1 < 4) {
+            if (numPeople > 1) {
+                break;
+            }
+            System.out.println("Ошибка. Невозможно разделить чек на данное колличество людей. Попробуйте снова.");
+            numPeople = scan.nextInt();
+        }
+        double check = item.calculator()/numPeople;
+        System.out.println("Каждый человек должен заплатить: " + check);
+        }
+    }
+
+
+
+
